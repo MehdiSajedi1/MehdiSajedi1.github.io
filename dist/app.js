934724bb81f56edd1c8a60d3efd4dd8748c211a2
Mehdi Sajedi <mehdiisajedi@gmail.com>
2021-08-17 20:34:22 -0700

    Added hero and projects sections

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+const viewProjectsBtn = document.querySelector('#view-projects');
+const heroText = document.querySelector('#hero-text');
+const projects = document.querySelector('#projects');
+
+viewProjectsBtn.addEventListener('click', (e) => {
+ 
+});
